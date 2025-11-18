672c3acb515ad498c2c40e4e2a6b99465437b900
yogeesh <yogeeshhegde778@gmail.com>
2025-11-18 12:57:20 +0530

    Update yml9

--- app/config.py
+++ app/config.py
@@ -7,19 +7,19 @@ ALGORITHM = os.getenv("ALGORITHM")
 EXPIRE_MINUTES = int(os.getenv("EXPIRE_MINUTES"))
 # DATABASE_URL = os.getenv("DATABASE_URL")
 
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_USER = os.getenv("DB_USER")
-DB_PORT = os.getenv("DB_PORT")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+# DB_USER = os.getenv("DB_USER")
+# DB_PORT = os.getenv("DB_PORT")
 
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
+# DB_NAME = os.getenv("DB_NAME")
+# DB_HOST = os.getenv("DB_HOST")
 
 
-TEST_DB_PASSWORD = os.getenv("TEST_DB_PASSWORD")
-TEST_DB_USER  = os.getenv("TEST_DB_USER")
-TEST_DB_PORT  = os.getenv("TEST_DB_PORT")
-TEST_DB_NAME  = os.getenv("TEST_DB_NAME")
-TEST_DB_HOST  = os.getenv("TEST_DB_HOST")
+# TEST_DB_PASSWORD = os.getenv("TEST_DB_PASSWORD")
+# TEST_DB_USER  = os.getenv("TEST_DB_USER")
+# TEST_DB_PORT  = os.getenv("TEST_DB_PORT")
+# TEST_DB_NAME  = os.getenv("TEST_DB_NAME")
+# TEST_DB_HOST  = os.getenv("TEST_DB_HOST")
 
 # DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 # DATABASE_URL_TEST =  f"postgresql://{TEST_DB_USER}:{TEST_DB_PASSWORD}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
