f2a2c6db6bb41a11ed9bd897abe28308578b137c
yogeesh <yogeeshhegde778@gmail.com>
2025-11-19 10:49:32 +0530

    update yml,cnfig adn .env

--- app/config.py
+++ app/config.py
@@ -18,7 +18,6 @@
 
 # DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-
 import os
 from dotenv import load_dotenv
 
@@ -28,12 +27,11 @@ SECREATE_KEY = os.getenv("SECREATE_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 EXPIRE_MINUTES = int(os.getenv("EXPIRE_MINUTES", 30))
 
-# 1️⃣ PRIORITY: if CI test database URL exists → use it
+# If CI is running, use test DB URL
 DATABASE_URL_TEST = os.getenv("DATABASE_URL_TEST")
-if DATABASE_URL_TEST:
-    DATABASE_URL = DATABASE_URL_TEST
 
-# 2️⃣ ELSE → use local DB configuration
+if DATABASE_URL_TEST and DATABASE_URL_TEST != "":
+    DATABASE_URL = DATABASE_URL_TEST
 else:
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_USER = os.getenv("DB_USER")
@@ -41,4 +39,6 @@ else:
     DB_NAME = os.getenv("DB_NAME")
     DB_HOST = os.getenv("DB_HOST")
 
-    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DATABASE_URL = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
